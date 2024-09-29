056a8ef6679b1c312398c66c3c089d70f1335283
KojoFocus <ernestobimpeh9@gmail.com>
2024-09-29 17:49:02 +0000

    added the filter functionality to the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
 import { useState } from "react";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  // Initialize the list of expenses with some sample data
   const [expenses, setExpenses] = useState([
     { id: 1, description: "Groceries", amount: 100, category: "groceries" },
     { id: 2, description: "Rent", amount: 1500, category: "rent" },
     { id: 3, description: "Gas", amount: 100, category: "gas" },
-    { id: 4, description: "Entertainment", amount: 50, category: "entertainment" },
+    {
+      id: 4,
+      description: "Entertainment",
+      amount: 50,
+      category: "entertainment",
+    },
     { id: 5, description: "Utilities", amount: 20, category: "utilities" },
   ]);
 
   // Handle deleting an expense by its id
   const handleDelete = (id: number) => {
-    console.log("Deleting expense with id:", id);
-    setExpenses((prevExpenses) => prevExpenses.filter((expense) => expense.id !== id));
+    setExpenses((prevExpenses) =>
+      prevExpenses.filter((expense) => expense.id !== id)
+    );
   };
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
   return (
     <div>
       <ExpenseForm />
-      <ExpenseList 
-        onDelete={handleDelete}  // Pass the delete handler function
-        expenses={expenses}      // Pass the current list of expenses
+      <ExpenseFilter
+        onSelectCategory={(category) => setSelectedCategory(category)}
+      />
+      <ExpenseList
+        onDelete={handleDelete} // Pass the delete handler function
+        expenses={visibleExpenses} // Pass the current list of expenses
       />
     </div>
   );
---
+++ src/components/ExpenseFilter.tsx
@@ -0,0 +1,18 @@
+interface Props{
+    onSelectCategory: (category: string)=>void;
+}
+
+const ExpenseFilter = ({onSelectCategory}: Props) => {
+  return (
+    <div className="px-5">
+    <select className="form-select" onChange={(event)=>onSelectCategory(event.target.value)} >
+        <option value="">All Categories</option>
+        <option value="groceries">Groceries</option>
+        <option value="utilities">Utilities</option>
+        <option value="entertainment">Entertainment</option>
+    </select>
+    </div>
+  )
+}
+
+export default ExpenseFilter
