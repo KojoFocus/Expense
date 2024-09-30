57a17efbbdb64b8ba5d909c912b98b7f5ab193d9
KojoFocus <ernestobimpeh9@gmail.com>
2024-09-30 07:32:04 +0000

    the onChange  event had been implemented in the ExpenseFilter

--- src/components/ExpenseFilter.tsx
+++ src/components/ExpenseFilter.tsx
@@ -1,18 +1,21 @@
-interface Props{
-    onSelectCategory: (category: string)=>void;
+interface Props {
+  onSelectCategory: (category: string) => void;
 }
 
-const ExpenseFilter = ({onSelectCategory}: Props) => {
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
     <div className="px-5">
-    <select className="form-select" onChange={(event)=>onSelectCategory(event.target.value)} >
+      <select
+        className="form-select"
+        onChange={(event) => onSelectCategory(event.target.value)}
+      >
         <option value="">All Categories</option>
         <option value="groceries">Groceries</option>
         <option value="utilities">Utilities</option>
         <option value="entertainment">Entertainment</option>
-    </select>
+      </select>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseFilter
+export default ExpenseFilter;
